4bcfd2419a339620a39c6fba8fee18b8bb2c9168
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-06-16 15:12:24 +0200

    gitignore

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 #! /usr/bin/env node
 
+const argv = require('minimist')(process.argv.slice(2));
+const package = require('./package');
+
+if (argv.v || argv.version) {
+    console.log(package.version);
+    return;
+}
+
+if (argv.h || argv.help) {
+    console.log(`Usage: a [-v] [-h] <resource> <verb> <param>
+-v: version
+-h: help`);
+    return;
+}
+
 console.log('Available resources');
 
