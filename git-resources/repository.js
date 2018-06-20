4790cdf00f36de83a63ea13d7d217e2d8b280ae5
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-06-20 18:48:56 +0200

    commit

--- git-resources/repository.js
+++ git-resources/repository.js
@@ -7,6 +7,7 @@ module.exports = {
     async create(name) {
         await execute(`git init ${name}`);
     },
+    retrieve: 'git remote show <repository>',
     async delete(name) {
         // check that name/.git exists.
         try {
