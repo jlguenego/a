4bf4c23603dd25ae9030a70877c6a993eade5aa2
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-06-22 18:34:38 +0200

    commit

--- git-resources/object.js
+++ git-resources/object.js
@@ -12,27 +12,16 @@ module.exports = {
   USAGE
 
     a object create <name>
-        Create a git branch: git branch -- <name>
+        
     
-    a branch retrieve <name>
-        Retrieve a git branch: git rev-parse <name>
+    a object retrieve <name>
+        Retrieve an object: git cat-file -p <sha1>
 
     a object                 
-        List all objects
+        List all objects: git rev-list --objects --all
 `),
     create: async (name) => {
-        if (isRemote(name)) {
-            const [remote, branch] = name.split('/');
-            const localBranches = await getLocalBranches();
-            if (localBranches.includes(branch)) {
-                await execute(`git push -u ${remote} ${branch}`);
-                return;
-            }
-            // if local branch does not exist with the same name, throw an error (political choice).
-            console.error(`Politic choice: you must have a local branch with the same name.`);
-            process.exit(1);
-        }
-        await execute(`git branch -- ${name}`)
+        
     },
     retrieve: 'git cat-file -p <sha1>',
     list: 'git rev-list --objects --all',
